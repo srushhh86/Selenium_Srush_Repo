e93660d1c6d3ff07238afd8b5a39bfdd02553744
Ankitha <Ankitha@DESKTOP-E83J03S>
2025-07-01 13:13:52 +0530

    addednewtest commit

---
+++ DemoWebsShop/src/test/java/srushti/Srush_Test.java
@@ -0,0 +1,13 @@
+package srushti;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Srush_Test {
+	@Test
+	public void run() {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
